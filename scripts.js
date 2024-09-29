79242200671897b0f7886dd59a45af08a97252fb
Ruan Manoel <ruuanmanoel@gmail.com>
2024-09-29 18:46:42 -0300

    :sparkles: feat: funcionalidades da calculadora - operações e digitos

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function adicionarCaracter(caracter){
+    const valorInput = document.querySelector('.display').value
+    document.querySelector('.display').value += caracter
+    console.log(valorInput)
+}
+function limparTela(){
+    document.querySelector('.display').value = ''
+}
+function calcular(){
+  const valorInput = document.querySelector('.display').value
+  document.querySelector('.display').value = eval(valorInput)
+
+}
+function inverte(){
+    const valorInput = document.querySelector('.display').value
+
+    document.querySelector('.display').value = valorInput * -1;
+
+}
